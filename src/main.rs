f21b96705fe00c3c38411454e643807a83d1621d
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-09-24 11:58:49 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 use TerimalRtdm::*;
 //use rand::Rng;
 //rand::rng().random_range(0..=4);
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-
-fn rotate_right(shape: Vec<(i8, i8)>) -> Vec<(i8, i8)> {
-    for i in 0..shape.len() {
-        if shape.0 > 0 {
-            shape.0 = -shape.0;
-        } else if shape.0 < 0 {
-
-        }
-    }
-}
 
 fn main() {
     let mut app = App::new();
@@ -22,7 +12,6 @@ fn main() {
     show_cursor(false);
 
     let tick_delay = Mutex::new(1.0);
-    let highscore = Mutex::new(0); //  https://doc.rust-lang.org/book/ch16-03-shared-state.html
     // 20 rows, and 10 cols
     let mut grid: Vec<Vec<bool>> = Vec::new();
     for _ in 0..21 {
@@ -30,11 +19,17 @@ fn main() {
     }
 
     thread::spawn(move || {
+        let mut app1 = App::new();
+        let mut grid_clone = grid.clone();
+        Text::new().show(&mut app1, "test", pos!(0, 0));
+        for i in 0..grid_clone.len() {
+            for j in 0..grid_clone[i].len() {
+                let pixel = if grid_clone[i][j] { "🔳" } else { "⬛️" };
+                Text::new().show(&mut app1, "test", pos!(i, j));
+            }
+        }
         thread::sleep(Duration::from_secs_f64(*tick_delay.lock().unwrap()));
     });
-    
-    let shape_falling: bool = false;
-    let cur_shape: Vec<(i8, i8)> = Vec::new();
 
     loop {
         if Key::o().pressed(&mut app, KeyType::Esc) {
